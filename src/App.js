0b0e35fd07f1948bd9f25600b9f0c3e057f94f40
angel melendez <angelmelen88@gmail.com>
2023-05-29 13:53:47 -0600

    primeros pasos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './platzi.webp';
+// import logo from './platzi.webp';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <TodoItem/>
+    <TodoItem/>
+    <TodoItem/>
+    </>
+  
+    
+  );
+}
+
+function TodoItem () {
+  return (
+    <li>
+      <span>v</span>
+      <p>llorar con la llorona</p>
+      <span>x</span>
+    </li>
   );
 }
 
