af560b486a5e4a48c8755d833e2cf6f670a4279c
angel melendez <angelmelen88@gmail.com>
2023-06-03 16:19:27 -0600

    clase 6 eventos en react, todo bien aqui

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,6 +2,15 @@ import './CreateTodoButton.css'
 
 export function CreateTodoButton () {
     return (
-      <button className='CreateTodoButton'>+</button>
+      <button 
+        className='CreateTodoButton'
+        onClick={
+          (event) => {
+            console.log('le diste click');
+            console.log(event);
+            console.log(event.target);
+          }
+        }
+      >+</button>
     );
   }
